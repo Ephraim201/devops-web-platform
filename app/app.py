f291b0685febdf3bd89d3bdeef099af19293d056
Ephraim201 <williampena5336@gmail.com>
2026-08-10 13:10:36 +0200

    kubernetes parte 1

--- app/app.py
+++ app/app.py
@@ -1,11 +1,18 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
+message = os.getenv(
+    "APP_MESSAGE",
+    "DevOps Web Platform funcionando correctamente"
+)
+
 
 @app.route("/")
 def home():
-    return "DevOps Web Platform funcionando correctamente"
+    return message
 
 
 if __name__ == "__main__":
